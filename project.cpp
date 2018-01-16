5e7cecc3b4bef0f86afedb4d286f128e81d1e1be
Catalin <catalin.ciobirca@yahoo.com>
2018-01-16 20:42:34 +0200

    xml parser...

--- project.cpp
+++ project.cpp
@@ -1,5 +1,7 @@
 #include "project.h"
 
+#include <QDir>
+
 Project::Project(QObject *parent)
     : QObject(parent)
 {
@@ -9,4 +11,35 @@ Project::Project(QObject *parent)
 Project::~Project()
 {
 
+}
+
+QString Project::projectName()
+{
+    return m_projectName;
+}
+
+QString Project::projectDirectory()
+{
+    return m_projectDirectory;
+}
+
+QString Project::buildType()
+{
+    return m_buildType;
+}
+
+void Project::setProjectStructure(const QString &projectName, const QString &workingDirectory, const QString &buildType)
+{
+    QDir projDirectory(workingDirectory + "/" + projectName);
+    //QDir sourceDirectory(workingDirectory + "/" + projectName + "/" + projectName);
+    QDir buildDirectory(workingDirectory + "/" + projectName + "/build_" + buildType);
+
+    if(!buildDirectory.exists())
+    {
+        projDirectory.mkdir(buildDirectory.dirName());
+    }
+
+    m_projectName = projectName;
+    m_projectDirectory = projDirectory.absolutePath();
+    m_buildType = buildType;
 }
--- project.h
+++ project.h
@@ -10,6 +10,12 @@ public:
     Project(QObject *parent = nullptr);
     ~Project();
 
+    QString projectName();
+    QString projectDirectory();
+    QString buildType();
+
+    void setProjectStructure(const QString &projectName, const QString &workingDirectory, const QString &buildType);
+
 private:
     QString m_projectName;
     QString m_projectDirectory;
